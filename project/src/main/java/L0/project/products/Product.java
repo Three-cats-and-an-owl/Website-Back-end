04007dab844e28fcfee59a0f827a48dfa7c49fba
Jose Lee <andrelee089@gmail.com>
2024-04-21 17:52:48 -0700

    Match product class with database product

--- project/src/main/java/L0/project/products/Product.java
+++ project/src/main/java/L0/project/products/Product.java
@@ -3,9 +3,6 @@ package L0.project.products;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,36 +10,25 @@ import java.util.*;
 
 @Document(collection = "products")
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 
 public class Product {
     @Id
     private ObjectId id;
-    private String name;
-    private String description;
-    private String category;
+    private String product_name;
+    private String product_url;
+    private String product_type;
+    private List<String> clean_ingredients;
     private String price;
-    private String image;
-    private List<String> tags;
-    private List<String> colors;
-    private List<String> sizes;
-    private List<String> images;
-    private List<String> reviews;
     
     // Constructor
-    public Product(ObjectId id, String name, String description, String category, String price, String image, List<String> tags, 
-    List<String> colors, List<String> sizes, List<String> images, List<String> reviews) {
+    public Product(ObjectId id, String product_name, String product_url, String product_type, List<String> clean_ingredients, String price){
         this.id = id; //This is the ID value being passed into ProductRepository to locate our product.
-        this.name = name;
-        this.description = description;
-        this.category = category;
+        this.product_name = product_name;
+        this.product_url = product_url;
+        this.product_type = product_type;
+        this.clean_ingredients = clean_ingredients;
         this.price = price;
-        this.image = image;
-        this.tags = tags;
-        this.colors = colors;
-        this.sizes = sizes;
-        this.images = images;
-        this.reviews = reviews;
+
     }
 }
