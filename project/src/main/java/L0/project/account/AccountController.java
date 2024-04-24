d3bb8e54dc5ec22027001b9ead6edb801d913d7a
Jose Lee <andrelee089@gmail.com>
2024-04-24 14:35:39 -0700

    endpoint rename

--- project/src/main/java/L0/project/account/AccountController.java
+++ project/src/main/java/L0/project/account/AccountController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/v1/accounts")
 public class AccountController {
 
     @Autowired
--- project/src/main/java/L0/project/products/ProductController.java
+++ project/src/main/java/L0/project/products/ProductController.java
@@ -30,5 +30,4 @@ public class ProductController {
             return ResponseEntity.notFound().build();
         }
     }
-    
 }
